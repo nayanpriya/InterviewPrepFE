1580b68a9e499067c98eb6e2ce22d7002953b97a
Nayan Priya <nnayanpriya@gmail.com>
2020-07-24 12:34:27 +0530

    review changes

---
+++ BlockDisplay/index.js
@@ -0,0 +1,9 @@
+document.getElementById("position_div").onclick = () => {
+
+    positionDivs()
+};
+
+function positionDivs() {
+    document.getElementById("parent").style = `position: relative ; left : 30%;`;
+
+}
