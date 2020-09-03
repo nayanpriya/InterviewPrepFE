e6dcce7e08ed2950de37896785c88dc07d5d9771
Nayan Priya <nnayanpriya@gmail.com>
2020-09-03 13:59:36 +0530

    reactassignment

---
+++ ReactAssignment/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        const users = [
+            { name: 'Robin' },
+            { name: 'Markus' },
+        ];
+
+        return (
+            <ul>
+                {users.map(user => <li>{user.name}</li>)}
+            </ul>
+        );
+    }
+}
